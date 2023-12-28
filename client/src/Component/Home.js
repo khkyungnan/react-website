e8f49332dc0d6673cabf23eeaad56947af4bea2c
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-28 16:46:00 +0900

    FF

--- client/src/Component/Home.js
+++ client/src/Component/Home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Weather from './Weather/Weather';
+import MusicPlayer from './Music/MusicPlayer';
 
 const Home = () => {
   return (
     <Container>
       <Row>
         <Col>
+          <Weather />
+          <MusicPlayer />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <Card>
             <Card.Body>
               <Card.Title>리액트 웹사이트</Card.Title>
               <Card.Text>리액트와 부트스트랩을 활용한 웹사이트</Card.Text>
+
               <Link to="/movie">
                 <Button variant="primary">Go To Movies</Button>
               </Link>
